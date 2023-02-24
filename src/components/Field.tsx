6101954e59bb8a36b4b802aeab437a7288b96666
Ivan <74314908+intelligent-person@users.noreply.github.com>
2023-02-24 12:16:57 +0100

    Update Field.tsx

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -2,19 +2,19 @@ import useFieldsStore from "../store/fieldsStore";
 import {FC, useCallback} from "react";
 
 interface FieldProps {
-		id: number
+  id: number
 }
 
 export const Field: FC<FieldProps> = ({ id }) => {
-	const { onHover, hoverFields, isHoverMode } = useFieldsStore()
+  const { onHover, hoverFields, isHoverMode } = useFieldsStore()
 
-	const onHoverField = useCallback(() => {
-		if (isHoverMode) onHover(id)
-		else return null
-	}, [id, isHoverMode])
+  const onHoverField = useCallback(() => {
+    if (isHoverMode) onHover(id)
+    else return null
+  }, [id, isHoverMode])
 
-	return <div
-		className={`field${hoverFields.has(id) ? ' hover' : ''}`}
-		onMouseEnter={onHoverField}
-	></div>
+  return <div
+    className={`field${hoverFields.has(id) ? ' hover' : ''}`}
+    onMouseEnter={onHoverField}
+  ></div>
 }
